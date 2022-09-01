b7bf653b134054e40be98b8afa520eb6c4fea5b7
Marty McGee <support@companyjuice.com>
2022-09-01 12:49:17 -0700

    v0.0.4 threed: index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // simple try:catch promise
 try {
-  console.log("threed")
+  console.log("threed index.js loaded")
 } catch (err) {
   // an error has occurred. no output is being attempted here.
+  // console.log("threed index.js not loaded")
 }
