712dc17155be0961b50ae045d6e930d53a109bf4
Marty McGee <support@companyjuice.com>
2022-09-03 14:18:47 -0700

    v0.0.11 threed: vercel deploy 'next build'

--- vite.config.js
+++ vite.config.js
@@ -18,8 +18,8 @@ export default defineConfig({
 
   resolve: {
     alias: {
-      '~': path.resolve(__dirname, './src'),
-      '@': path.resolve(__dirname, './src')
+      '~': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src')
     },
   }
 })
