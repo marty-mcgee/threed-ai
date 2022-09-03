8e2a4ca5887af1236a36a17674cb8a63ab024371
Marty McGee <support@companyjuice.com>
2022-09-03 13:47:21 -0700

    v0.0.11 threed: vercel deploy 'next build'

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,43 +1,16 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
-import { setState } from 'src/stores/store'
-import Header from 'src/components/seo/config'
-import Dom from 'src/components/layout/dom'
-import 'src/styles/index.css'
+import { setState } from './src/stores/store'
+import Header from './src/components/seo/config'
+import Dom from './src/components/layout/dom'
+import './src/styles/index.css'
 
 // ==============================================================
 // CUSTOM IMPORT (SSR true|false)
-const LCanvas = dynamic(() => import('src/components/layout/canvas'), {
+const LCanvas = dynamic(() => import('./src/components/layout/canvas'), {
   ssr: true,
 })
-
-// ==============================================================
-// GRAPHQL -- APOLLO (SERVER, NOT CLIENT)
-// const doBootApollo = true
-
-// if (doBootApollo) {
-//   const { ApolloServer } = require('apollo-server')
-//   const gql = require('graphql-tag')
-
-//   const typeDefs = gql`
-//     type Query {
-//       word:  String
-//     }
-//   `
-
-//   const resolvers = {
-//     Query: {
-//       word: () => "HEY HEY HEY"
-//     }
-//   }
-
-//   const schema = new ApolloServer({ typeDefs, resolvers })
-
-//   schema.listen({ port: process.env.PORT }).then(({ url }) => {
-//     console.debug(`schema ready at ${url}`)
-//   })
-// }
 
 // ==============================================================
 // NEXT APP
