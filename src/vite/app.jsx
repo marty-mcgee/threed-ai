a3ca98a89b92d099232c6e76e05d6e695993e1c9
Marty McGee <support@companyjuice.com>
2022-10-19 16:29:56 -0700

    v0.1.3 threed-ai: r3fCanvas Box box .jsx

--- src/vite/app.jsx
+++ src/vite/app.jsx
@@ -1,53 +1,51 @@
+// ==============================================================
+// RESOURCES
+
+// ** NextJS Imports
+import Image from "next/image"
+
+// ** React Imports
 import React, { useState, useEffect } from "react"
+
+// ** Router Imports
 import { Router, Link } from "wouter"
 
-// ==============================================================
-/**
-* This code defines the react app
-*
-* Imports the router functionality to provide page navigation
-* Defines the Home function outlining the content on each page
-* Content specific to each page (Home and About) is defined in their components in /pages
-* Each page content is presented inside the overall structure defined here
-* The router attaches the page components to their paths
-*/
-
-// Import and apply CSS stylesheet
-import "~/styles/styles.css"
+// ** CSS Imports
+import styles from "~/styles/styles.css"
 
 // Where all of our pages come from
 import PageRouter from "~/components/router/router.jsx"
 
 // The component that adds our meta tags to the page
-import Seo from '~/components/seo/seo.jsx'
+import Seo from "~/components/seo/seo.jsx"
 
 // ==============================================================
 // REACT APP
-// Home function that is reflected across the site
+
+// Home 'App' function that is reflected across the site
 export default function Home() {
   return (
     <Router>
       <Seo />
-      <main role="main" className="wrapper">
+      <main role="main" className={styles.wrapper}>
         <div className="content">
           {/* Router specifies which component to insert here as the main content */}
           <PageRouter />
         </div>
       </main>
       {/* Footer links to Home and About, Link elements matched in router.jsx */}
-      <footer className="footer">
-        <div className="links">
+      <footer className={styles.footer}>
+        <div className={styles.links}>
           <Link href="/">Home</Link>
-          <span className="divider">|</span>
+          <span className={styles.divider}>|</span>
           <Link href="/about">About</Link>
         </div>
         <a
-          className="btn--remix"
+          className={styles["btn--remix"]}
           target="_top"
           href="https://glitch.com/edit/#!/remix/glitch-hello-react"
         >
-          <img src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="" />
-          Remix on Glitch
+          <Image src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="threed.ai on glitch" />
         </a>
       </footer>
     </Router>
